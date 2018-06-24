bc904fd6b180862cd853af355f4660e02e7b548a
Marc Gela <mgelabert3@gmail.com>
2018-06-24 12:30:21 +0200

    hiding cards if no pair

--- memorygame/Memorygame.swift
+++ memorygame/Memorygame.swift
@@ -12,11 +12,25 @@ class Memorygame
 {
     var cards = [Card]()
     
-    func chooseCard(at index: Int){
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                for flipdownIndex in cards.indices {
+                    cards[flipdownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
     }
     
